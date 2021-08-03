82598f74fe704b4d71ce972e8b8e75f4dc1b535e
Derrick Hudson <hudsonds@appstate.edu>
2021-08-02 22:01:51 -0400

    ready for first tests

--- assignment/arrays.cc
+++ assignment/arrays.cc
@@ -1,27 +1,18 @@
 #include <stdio.h>
 
-double getPositiveAverage(double[])
+double getPositiveAverage(double myArray[], int numItems)
 {
-    int n[10]; /* n is an array of 10 integers */
-    int i, j;
-
-    /* initialize elements of array n to 0 */
-    for (i = 0; i < 10; i++)
-    {
-        n[i] = i + 100; /* set element at location i to i + 100 */
-    }
-
-    /* output each array element's value */
-    for (j = 0; j < 10; j++)
-    {
-        printf("Element[%d] = %d\n", j, n[j]);
-    }
-
-    return 0;
 }
 
-double countRangeValues(double[], int, double)
+int countRangeValues(double arr[], int numItems, double entry)
 {
+    int count = 0;
+    for (int i = 0; i < numItems; i++)
+    {
+        if (arr[i] <= (entry - 0.5) || arr[i] > (entry + 0.5))
+            count++;
+    }
+    return count;
 }
 
 double getMaxAbsolute(double[], int)
--- assignment/main.cc
+++ assignment/main.cc
@@ -1,11 +1,14 @@
 #include <stdio.h>
 
-double getPositiveAverage(double[]);
-double countRangeValues(double[], int, double);
+double getPositiveAverage(double[], int);
+int countRangeValues(double[], int, double);
 double getMaxAbsolute(double[], int);
 int countInverses(int[], int);
 double getMaxCount(double[], int);
 
 int main()
 {
+    double arr[] = {1.1, 1.2, 1.5, 1.8, 2.0, 2.1, 2.2, 2.4, 2.7, 3.0, 3.1, 3.2, 3.5, 3.7};
+
+    countRangeValues(arr, 14, 2.5);
 }
