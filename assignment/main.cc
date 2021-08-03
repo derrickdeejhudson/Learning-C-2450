f4df89372923a945a158f23ac87800251499d199
Derrick Hudson <hudsonds@appstate.edu>
2021-08-02 21:17:26 -0400

    just starting

---
+++ assignment/main.cc
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+double getPositiveAverage(double[]);
+double countRangeValues(double[], int, double);
+double getMaxAbsolute(double[], int);
+int countInverses(int[], int);
+double getMaxCount(double[], int);
+
+int main()
+{
+}
